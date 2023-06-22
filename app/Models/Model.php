02f968ff56ad78fdb86d6095b3a132c463136475
Edo dewa <Edo dewa>
2023-06-22 20:34:49 +0700

    improve

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Makul extends Model
 {
     use HasFactory;
-    public $table = "makuls"; // custom nama tabel
+    public $table = "makul"; // custom nama tabel
     protected $primaryKey = "kode_kelas"; // custom primary key
     public $incrementing=false; // mematikan auto increment
     protected $keyType="string"; // custom type primary key (defaultnya autoincreme
